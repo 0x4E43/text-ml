d1ef7e56af40ccdf7f0696c868af01ce948ff69c
0x4E43 <nimaic.dev@gmail.com>
2024-05-26 11:20:38 +0530

    File Upload works

--- textml/views.py
+++ textml/views.py
@@ -1,25 +1,26 @@
+import csv
+from io import FileIO, TextIOWrapper
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 
-def index(request):
-    try:
-        return render(request, 'index.html')
-    except Exception as e:
-        print(e)
-        print("Error rendering template: {}", e)
+import pandas as pd
 
-def upload(request):
+# def index(request):
+#     try:
+#         return render(request, 'index.html')
+#     except Exception as e:
+#         print(e)
+#         print("Error rendering template: {}", e)
+
+def index(request):
     if request.method == 'POST':
         try:
             myfile = request.FILES['file-upload']
             print("Received file:", myfile.name)
-            fs = FileSystemStorage()
-            filename = fs.save(myfile.name, myfile)
-            uploaded_file_url = fs.url(filename)
-            print("Uploaded file URL:", uploaded_file_url)
-            fileurl = '.' + uploaded_file_url
-            print("File URL on server:", fileurl)
+            csv_data = pd.read_csv(myfile)
+            print(csv_data.head(5))
+            return render(request, 'index.html', {"upload": "true"})
         except Exception as e:
             print("Error uploading file:", e)
     else:
